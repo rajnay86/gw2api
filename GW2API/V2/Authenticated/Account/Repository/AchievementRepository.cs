4c7965e4bd9e1b65d592cd49e177175f3a39a0e8
rajnay86@gmail.com <rajnay86@gmail.com>
2017-10-14 13:24:00 -0400

    Add account achievements

---
+++ GW2API/V2/Authenticated/Account/Repository/AchievementRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using GW2Api.Common;
+using GW2API.V2.Authenticated.Account.Models;
+using RestSharp;
+
+namespace GW2API.V2.Authenticated.Account.Repository
+{
+    public class AchievementRepository
+    {
+        public static List<Achievement> GetAchievements(string apiKey)
+        {
+            var client = new GW2ClientAuthorized(2, apiKey);
+            var request = new RestRequest("account/achievements");
+            var response = client.Execute<List<Achievement>>(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+            {
+                return null;
+            }
+            return response.Data;
+        }
+    }
+}
